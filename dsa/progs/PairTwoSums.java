30708fe4b9b21faf15fd383d9ea8698604573824
raj2684 <57202126+raj2684@users.noreply.github.com>
2024-07-22 19:46:56 +0530

    Add new java program to pair two sums

---
+++ dsa/progs/PairTwoSums.java
@@ -0,0 +1,31 @@
+package dsa.progs;
+
+public class PairTwoSums {
+    static boolean chkPair(int[] arr, int size, int x)
+    {
+        for (int i = 0; i < (size - 1); i++) {
+            for (int j = (i + 1); j < size; j++) {
+                if (arr[i] + arr[j] == x) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+
+        int[] arr = { 0, -1, 2, -3, 1 };
+        int x = -2;
+        int size = arr.length;
+
+        if (chkPair(arr, size, x)) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
